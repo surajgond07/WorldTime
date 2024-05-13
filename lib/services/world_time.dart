0375a5b7b0af928e996a6a937965d22c261f94dc
Suraj Gond <surajgond07@gmail.com>
2024-05-13 07:03:07 +0530

    Error solved for not show correct time

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -17,26 +17,25 @@ class WorldTime {
       http.Response response = await http
           .get(Uri.parse('https://worldtimeapi.org/api/timezone/$url'));
       Map data = jsonDecode(response.body);
-      // print(data);
+
       // get properties from data
       String dateTime = data['datetime'];
-      String offset = data['utc_offset'].substring(1, 3);
-      print('DateTime: $dateTime');
-      print('offset: $offset');
+      String offset = data['utc_offset'];
 
-      // create a dateTime object
+      // create a DateTime object
       DateTime now = DateTime.parse(dateTime);
-      now = now.add(Duration(hours: int.parse(offset)));
-      print('Now Edited Time is: $now');
+      now = now.toUtc(); // Convert datetime to UTC
+      now = now.add(Duration(
+          hours: int.parse(offset.split(':')[0]),
+          minutes: int.parse(offset.split(':')[1]))); // Add the offset directly
 
-      // formate time
-      // ternary operator
-      isDayTime = now.hour > 6 && now.hour < 20 ? true : false;
+      // format time
+      isDayTime = now.hour > 6 && now.hour < 20;
       time = DateFormat.jm()
-          .format(now); // DateFormate function provide by intl package
+          .format(now); // Use DateFormat from intl package to format time
     } catch (error) {
       print('Error fetching data: $error');
-      time = "Could not get time data ";
+      time = "Could not get time data";
     }
   }
 }
