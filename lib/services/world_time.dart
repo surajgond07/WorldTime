b2dc6f7e509fc376611c8d6594a9532cd78806be
Suraj Gond <surajgond07@gmail.com>
2024-05-15 12:05:22 +0530

    Handle no internet connection

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -7,12 +7,23 @@ class WorldTime {
   String time = ''; // the time in that location
   String flag; // url for an assets flat icon
   String url; // location url for the API endpoint
-  bool isDayTime = true; // tru of false is day time
+  bool isDayTime = true; // true or false for day time
 
   WorldTime({required this.location, required this.flag, required this.url});
 
   Future<void> getTime() async {
-    //  make the request
+    // Check for network connectivity
+    bool isConnected = await http.Client()
+        .get(Uri.parse('https://google.com'))
+        .then((response) => true)
+        .catchError((error) => false);
+
+    if (!isConnected) {
+      time = "No internet connection";
+      return;
+    }
+
+    // Make the request
     try {
       http.Response response = await http
           .get(Uri.parse('https://worldtimeapi.org/api/timezone/$url'));
@@ -34,7 +45,7 @@ class WorldTime {
       time = DateFormat.jm()
           .format(now); // Use DateFormat from intl package to format time
     } catch (error) {
-      // print('Error fetching data: $error');
+      // Handle other errors
       time = "Could not get time data";
     }
   }
